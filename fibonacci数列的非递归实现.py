7b0dd7276a0aa28c115d6e3e87014992acd38829
lrc <907867085@qq.com>
2017-04-06 20:57:36 +0800

    change

--- fibonacci数列的非递归实现.py
+++ fibonacci数列的非递归实现.py
@@ -3,26 +3,10 @@
 斐波那切数列的非递归实现
 现在要求输入一个整数n，请你输出斐波那契数列的第n项。
 '''
-
 class Solution:
-    def fibonacciateArr(self,n):
-        first,second = 0,1
-        fibN = 0
-        count = 2
-        n = int(n)
-        if n < 3:
-            print 'input error'
-        else:
-            while count < n:
-                fibN = fibN + second
-                first = second
-                second = fibN
-                count += 1
-            print fibN
-            
-s = Solution()
-s.fibonacciateArr(-2)
-s.fibonacciateArr(1)
-s.fibonacciateArr(2)
-s.fibonacciateArr(2.5)
-s.fibonacciateArr(6)
+    def Fibonacci(self, n):
+        tempArray = [0, 1]
+        if n >= 2:
+            for i in range(2, n+1):
+                tempArray[i%2] = tempArray[0] + tempArray[1]
+return tempArray[n%2]
